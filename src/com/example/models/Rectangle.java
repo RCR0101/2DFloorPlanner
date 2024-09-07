8fddd1e19992cbe73bf07847c29a74e6b239f9c3
RCR0101 <dalmia.aryan@gmail.com>
2024-09-08 02:27:36 +0530

    feat(rectangle): create a generic rectangle model

---
+++ src/com/example/models/Rectangle.java
@@ -0,0 +1,18 @@
+package models;
+
+import java.awt.Color;
+
+public class Rectangle {
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public Color color;
+    public Rectangle(int x, int y, int width, int height, Color color){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+}
