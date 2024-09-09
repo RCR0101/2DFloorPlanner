0e517085c4722d997eeb9c587ec4e1eb9f3fce35
Pratyush Nair <52929338+RedMiner2005@users.noreply.github.com>
2024-09-09 20:58:02 +0530

    fix: Save directory changed to run everywhere

--- src/com/example/services/Constants.java
+++ src/com/example/services/Constants.java
@@ -1,5 +1,7 @@
 package services;
 
+import java.nio.file.Paths;
+
 public class Constants {
-    static final String SAVE_PATH = "/Users/adalmia/Documents/projects/java_dev/2DFloorPlanner/saves/saveFileTest.txt";
+    static final String SAVE_PATH = String.valueOf(Paths.get(Paths.get(System.getProperty("user.dir")).toString(), "saves", "saveFileTest.txt"));
 }
