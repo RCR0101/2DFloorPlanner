2fa5707009377ffb440a41036a36577fadd73164
RCR0101 <dalmia.aryan@gmail.com>
2024-09-10 20:16:08 +0530

    fix(rooms):override equals method to compare room objects

--- src/com/example/models/Room.java
+++ src/com/example/models/Room.java
@@ -2,18 +2,39 @@ package models;
 
 import java.awt.Color;
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Room implements Serializable {
+    private static final long serialVersionUID = 6697947686724816858L;  // Use the serialVersionUID from the error
+
     public int x;
     public int y;
     public int width;
     public int height;
     public Color color;
-    public Room(int x, int y, int width, int height, Color color){
+
+    public Room(int x, int y, int width, int height, Color color) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return x == room.x &&
+               y == room.y &&
+               width == room.width &&
+               height == room.height &&
+               Objects.equals(color, room.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, width, height, color);
     }
 }
