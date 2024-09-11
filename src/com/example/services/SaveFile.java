36e7fc970da42e926f04cd252dd07b4ca69032e9
RCR0101 <dalmia.aryan@gmail.com>
2024-09-11 23:23:52 +0530

    feat(save file): added checks to improve save functionality and to handle multiple saves or overwriting save files

--- src/com/example/services/SaveFile.java
+++ src/com/example/services/SaveFile.java
@@ -4,25 +4,54 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-
 import models.Room;
-
 
 public class SaveFile {
     ArrayList<Room> roomList;
-    public SaveFile(ArrayList<Room> roomList){
+    private static boolean isUnsavedChanges = true;
+
+    public SaveFile(ArrayList<Room> roomList) {
         this.roomList = roomList;
     }
-    public void saveFile() throws IOException{
+
+    public void saveFile() throws IOException {
         NewFile nFile = new NewFile();
-        FileOutputStream fileOutputStream = new FileOutputStream(nFile.newSave());
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-        objectOutputStream.writeObject(roomList);
-        objectOutputStream.close();
+        String savePath = nFile.getSavePath();
+
+        if (nFile.isTempFile() || !nFile.isSavePathSet()) {
+            savePath = nFile.newSave();
+        }
+
+        if (savePath != null) {
+            FileOutputStream fileOutputStream = new FileOutputStream(savePath);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(roomList);
+            objectOutputStream.close();
+
+            isUnsavedChanges = false;
+        }
     }
 
-    public boolean isFileSaved() throws ClassNotFoundException, IOException{
-        ArrayList<Room> roomSaved = new LoadFile().getFile();
-        return roomSaved.equals(roomList);
+    public boolean isFileSaved() throws ClassNotFoundException, IOException {
+        NewFile nFile = new NewFile();
+        LoadFile lFile = new LoadFile();
+
+        if (nFile.isSavePathSet()) {
+            ArrayList<Room> roomSaved = lFile.getFile(nFile.getSavePath());
+            boolean isSaved = roomSaved.equals(roomList);
+
+            isUnsavedChanges = !isSaved;
+
+            return isSaved;
+        }
+        return false;
+    }
+
+    public static boolean hasUnsavedChanges() {
+        return isUnsavedChanges;
+    }
+
+    public static void markUnsavedChanges() {
+        isUnsavedChanges = true;
     }
 }
