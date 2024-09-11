86fca5549520dbcca0de1c671232d88f3b796957
RCR0101 <dalmia.aryan@gmail.com>
2024-09-11 23:28:10 +0530

    feat(load files): better handling of opening empty files as well as loading a different file

--- src/com/example/services/LoadFile.java
+++ src/com/example/services/LoadFile.java
@@ -1,23 +1,33 @@
 package services;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
-
 import models.Room;
-
 
 public class LoadFile {
     ArrayList<Room> roomList;
 
-    public ArrayList<Room> getFile() throws IOException, ClassNotFoundException{
-        NewFile nFile = new NewFile();
-        FileInputStream fileInputStream = new FileInputStream(nFile.newSave());
+    public ArrayList<Room> getFile(String filePath) throws IOException, ClassNotFoundException {
+        File file = new File(filePath);
+
+        if (file.length() == 0) {
+            return new ArrayList<>();
+        }
+
+        FileInputStream fileInputStream = new FileInputStream(filePath);
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
         @SuppressWarnings("unchecked")
         ArrayList<Room> roomList = (ArrayList<Room>) objectInputStream.readObject();
         objectInputStream.close();
         return roomList;
+    }
+
+    public ArrayList<Room> loadDifferentFile() throws IOException, ClassNotFoundException {
+        NewFile nFile = new NewFile();
+        String filePath = nFile.loadSave();
+        return getFile(filePath);
     }
 }
