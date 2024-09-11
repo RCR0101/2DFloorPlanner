375d1d9b00f0ac962e0eae5b67786d44e2c49655
RCR0101 <dalmia.aryan@gmail.com>
2024-09-11 23:19:24 +0530

    chore: deleted redundant file

--- src/com/example/services/Constants.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-import java.nio.file.Paths;
-
-public class Constants {
-    static final String SAVE_PATH = String.valueOf(Paths.get(Paths.get(System.getProperty("user.dir")).toString(), "saves", "saveFileTest.txt"));
-}
