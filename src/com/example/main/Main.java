d3088870e3775bd4b2b11fe908385535bd12050b
RCR0101 <dalmia.aryan@gmail.com>
2024-11-19 22:40:06 +0530

    chore: remove unused imports and empty files

--- src/com/example/main/Main.java
+++ src/com/example/main/Main.java
@@ -1,9 +1,7 @@
 package com.example.main;
 
 import com.example.frames.OuterFrame;
-import com.formdev.flatlaf.intellijthemes.FlatArcDarkIJTheme;
 import com.formdev.flatlaf.intellijthemes.FlatDraculaIJTheme;
-import com.formdev.flatlaf.intellijthemes.FlatSolarizedLightIJTheme;
 
 public class Main {
     public static void main(String[] args) {
